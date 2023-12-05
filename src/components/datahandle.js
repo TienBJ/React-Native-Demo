b09843dfdd7ff9597c2f1e08a22b37aedf8b7d2b
Tien-A <tienbluejay@gmail.com>
2023-12-05 08:29:57 +0700

    commit 1

---
+++ src/components/datahandle.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { View, Text } from "react-native";
+
+export default function datahandle() {
+  return (
+    <View>
+        <Text>Hello Ông nội!</Text>
+        <Text>Hello Ông ngoại!</Text>
+        <Text>Hello Ông Trùm!</Text>
+        <Text>Hello Ông gà!</Text>
+        
+    </View>
+  )
+}
